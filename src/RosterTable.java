078a70c1aed8164f448b449d4357fccb7f88a01e
Dion Pimentel <dionpimentel260@gmail.com>
2020-11-21 17:48:54 -0700

    java class for the roster table

---
+++ src/RosterTable.java
@@ -0,0 +1,24 @@
+package FinalProject;
+
+import javax.swing.*;
+import java.io.File;
+
+
+public class RosterTable extends JTable
+{
+    private Student[] rosterData;
+    private Student[] attendanceData;
+    public RosterTable(Student[] studentData, String[][] tableData, String[] tableHeaders)
+    {
+        super(tableData, tableHeaders);
+        rosterData = studentData;
+        isCellEditable(0,0);
+    }
+
+    @Override
+    public boolean isCellEditable(int row, int column)
+    {
+        return false;
+    }
+
+}
