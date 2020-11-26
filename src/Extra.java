10fe2fbb9b01dd346a24b4d9220d62ba60942141
Michaela Chen <mchen157@asu.edu>
2020-11-26 03:10:08 -0700

    Added the Extra Class (People not on the Roster)

---
+++ src/Extra.java
@@ -0,0 +1,21 @@
+
+public class Extra
+{
+	private String asurite;
+	private int minutes;
+	
+	public Extra(String asurite, int minutes)
+	{
+		this.asurite = asurite;
+		this.minutes = minutes;
+	}
+	
+	public int getMinutes()
+	{
+		return minutes;
+	}
+	public String getAsurite()
+	{
+		return asurite;
+	}
+}
