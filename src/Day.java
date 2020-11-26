2dfa6be68fb8a5c42ccc5928fc759d0434df1094
Michaela Chen <mchen157@asu.edu>
2020-11-26 03:08:41 -0700

    Added the Day Class

---
+++ src/Day.java
@@ -0,0 +1,40 @@
+//package FinalProject;
+
+
+public class Day
+{
+	private String date;
+	private int minutes;
+	
+	//Constructors
+	public Day(String date, int minutes)
+	{
+		this.date = date;
+		this.minutes = minutes;
+	}
+	public Day()
+	{
+		date = "01/11";
+		minutes = 0;
+	}
+	
+	//Getters
+	public String getDate()
+	{
+		return date;
+	}
+	public int getMinutes()
+	{
+		return minutes;
+	}
+	
+	//Setters
+	public void setDate(String d)
+	{
+		date = d;
+	}
+	public void setMinutes(int min)
+	{
+		minutes = min;
+	}
+}
