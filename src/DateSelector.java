1d64c1fd48f92a21adbce18a65c56fb6cbb90300
Michaela Chen <mchen157@asu.edu>
2020-11-29 14:12:07 -0700

    Included the DateSelector

---
+++ src/DateSelector.java
@@ -0,0 +1,70 @@
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.swing.*;
+
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilDateModel;
+
+public class DateSelector extends JDialog implements ActionListener
+{
+	JDatePickerImpl datePicker;
+	JButton button;
+	String date;
+	
+	public DateSelector()
+	{
+		super();
+		setModal(true);
+	}
+	
+	public void selectDate()
+	{
+		setLayout(new FlowLayout());
+		
+		//Create the DatePicker
+		UtilDateModel model = new UtilDateModel();
+		Properties properties = new Properties();
+		properties.put("text.today", "Today");
+		properties.put("text.month", "Month");
+		properties.put("text.year", "Year");
+		JDatePanelImpl datePanel = new JDatePanelImpl(model,properties);
+		datePicker = new JDatePickerImpl(datePanel, new DateFormatter());
+		
+		//Add confirmation button
+		button = new JButton("OK");
+		button.addActionListener(this);
+		
+		//display the DatePicker
+		add(datePicker);
+		add(button);
+        setSize(300,300);
+        setLocationRelativeTo(null);
+        setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		if (e.getSource() == button)
+		{
+			//Get information from calendar
+			Date calendar = (Date) datePicker.getModel().getValue();
+			int month = calendar.getMonth()+1;
+			int day = calendar.getDate();
+			date = month + "/" + day;
+			dispose();
+		}
+	}
+	
+	public String getDate()
+	{
+		return date;
+	}
+}
